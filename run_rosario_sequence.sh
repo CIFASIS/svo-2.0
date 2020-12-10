4a08a6ff7c869ee88722edd817ef808549f38230
Román Comelli <roman.comelli@gmail.com>
2020-12-10 09:36:42 -0300

    Add portability to run_rosario_sequence.sh

--- run_rosario_sequence.sh
+++ run_rosario_sequence.sh
@@ -51,8 +51,8 @@ BAG=$1
 function cleanup() {
   if [ -n "${CID}" ] ; then
     printf "\e[31m%s %s\e[m\n" "Cleaning"
-    docker container stop $CID > /dev/null
-    docker logs $CID > $(dirname $OUTPUT_FILE)/$(basename -s .txt $OUTPUT_FILE)_log.txt
+    docker stop $CID > /dev/null
+    docker logs $CID > $(dirname $OUTPUT_FILE)/$(basename $OUTPUT_FILE .txt)_log.txt
     docker rm $CID > /dev/null
     unset CID
   fi
